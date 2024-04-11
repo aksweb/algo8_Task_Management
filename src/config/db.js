ef8e48f9a4d1a43149c3b6e927962ae68bfef7fa
aksweb <21it3004@rgipt.ac.in>
2024-04-11 14:38:20 +0530

    conn fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,18 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require('mysql2')
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = db;
+pool.getConnection((err, conn) => {
+    if (err) console.log(err)
+    console.log("Connected successfully")
+})
+
+module.exports = pool.promise()
