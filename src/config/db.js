d17ea97fe30d323d5bc197cdda16645043de954a
aksweb <21it3004@rgipt.ac.in>
2024-04-11 20:46:10 +0530

    db commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'task_management_db',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = db;
